220f12a1c7efa2ddd7b8a8aa0ff6ac2898ddbd56
Mark Hollomon <mhhollomon@gmail.com>
2020-01-02 21:04:50 -0500

    tailwind conversion

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+        zIndex : {
+            '-1' : '-1',
+        },
+        fontSize : {
+            '7xl': '5rem',
+        }
+    }
+  },
+  variants: {},
+  plugins: [],
+  corePlugins: {
+      backgroundAttachment: false,
+  }
+}
