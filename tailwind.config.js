b093bce95885406dfbd8816431906d870163ee6d
Mark Hollomon <mhhollomon@gmail.com>
2020-01-08 13:33:23 -0500

    next round of ccs changes for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 module.exports = {
   theme: {
+    fontFamily : {
+        'sans' : ['Roboto', 'sans-serif' ],
+        'serif': ["Noto Serif", 'serif'  ]
+    },
     extend: {
         zIndex : {
             '-1' : '-1',
         },
         fontSize : {
             '7xl': '5rem',
+        },
+        colors : {
+            '2gray' : '#222222',
+            '9gray' : '#999999',
+            'cgray' : '#cccccc',
+            'egray' : '#eeeeee'
+        },
+        width : {
+            '70pct' : '70%',
+            '15pct' : '15%',
+        },
+        margin : {
+            '5pct' : '5%'
         }
     }
   },
